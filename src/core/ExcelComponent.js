0a8d5f0aa55c9fff8e1680deff5b970da1026023
katya <kshabaeva@mail.ru>
2020-06-19 09:02:57 +0800

    Framework layout

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,7 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
-// Return component' template
+// Return component template
     toHTML() {
         return '';
     }
