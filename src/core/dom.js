c359c81ad2f35fd172a6dde1d2038e7b9ca91db3
katya <kshabaeva@mail.ru>
2020-07-03 08:57:21 +0800

    Rows resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -46,6 +46,12 @@ class Dom {
     findAll(selector) {
         return this.$el.querySelectorAll(selector);
     }
+
+    css(styles = {}) {
+       Object.keys(styles).forEach((key) => {
+           this.$el.style[key] = styles[key];
+       });
+    }
 }
 
 $('div').html('<h1>Test</h1>').clear();
