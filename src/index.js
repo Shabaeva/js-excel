c0445ae6fc378970451c92cf48f47af99319fd0a
katya <kshabaeva@mail.ru>
2020-07-22 11:11:13 +0800

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {createStore} from '@core/createStore';
 import {rootReducer} from '@/redux/rootReducer';
 import {storage} from '@core/utils';
 import {initialState} from '@/redux/initialState';
-
+//
 const store = createStore(rootReducer, initialState);
 
 store.subscribe((state) => {
