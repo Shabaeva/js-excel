0eb6e6f8e8504c3408e9e1e627921b70ced13421
katya <kshabaeva@mail.ru>
2020-07-29 09:58:57 +0800

    Cell styles

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -5,6 +5,7 @@ import {resizeHandler} from '@/components/table/table_resize';
 import {isCell, matrix, shouldResize, nextSelector} from './table_functions';
 import {TableSelection} from '@/components/table/TableSelection';
 import * as actions from '@/redux/actions';
+import {defaultStyles} from '@/constants';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
@@ -38,14 +39,15 @@ export class Table extends ExcelComponent {
         this.$on('formula:done', () => {
            this.selection.current.focus();
         });
-        // this.$subscribe((state) => {
-        //     console.log('TableState', state);
-        // });
+        this.$on('toolbar:applyStyle', (style) => {
+            this.selection.applyStyle(style);
+        });
     }
 
     selectCell($cell) {
         this.selection.select($cell);
         this.$emit('table:select', $cell);
+        console.log($cell.getStyles(Object.keys(defaultStyles)));
     }
 
     async resizeTable(event) {
--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -15,37 +15,38 @@ function toButton(button) {
             `;
 }
 
-export function createToolbar(state) {
+export function createToolbar(s) {
     const buttons = [
         {
-            icon: 'format_align_left',
-            active: false,
             value: {textAlign: 'left'},
+            icon: 'format_align_left',
+            active: s['textAlign'] === 'left',
         },
         {
-            icon: 'format_align_center',
-            active: false,
             value: {textAlign: 'center'},
+            icon: 'format_align_center',
+            active: s['textAlign'] === 'center',
         },
         {
-            icon: 'format_align_right',
-            active: false,
             value: {textAlign: 'right'},
+            icon: 'format_align_right',
+            active: s['textAlign'] === 'right',
         },
         {
+            value: {fontWeight: s['fontWeight'] === 'bold'?'normal':'bold'},
             icon: 'format_bold',
-            active: state['fontWeight'] === 'bold',
-            value: {fontWeight: state['fontWeight'] === 'bold'?'normal':'bold'},
+            active: s['fontWeight'] === 'bold',
         },
         {
+            value: {fontStyle: s['fontStyle'] === 'italic'?'normal':'italic'},
             icon: 'format_italic',
-            active: false,
-            value: {fontStyle: 'italic'},
+            active: s['fontStyle'] === 'italic',
         },
         {
+            value: {textDecoration: s['textDecoration'] === 'underline'
+                    ?'normal':'underline'},
             icon: 'format_underlined',
-            active: false,
-            value: {textDecoration: 'underline'},
+            active: s['textDecoration'] === 'underline',
         },
     ];
     return buttons.map(toButton).join('');
